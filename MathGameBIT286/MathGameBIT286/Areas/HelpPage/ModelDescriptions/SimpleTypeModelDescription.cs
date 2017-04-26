caa8650be45dc927763de1c01feb0ad85d0783ce
HaakonJ <haakon@haakonjakobson.com>
2017-04-26 12:34:43 -0700

    redownload

--- MathGameBIT286/MathGameBIT286/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MathGameBIT286.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- MathGameBIT286/MathGameBIT286/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MathGameBIT286.WebApiApplication" Language="C#" %>
