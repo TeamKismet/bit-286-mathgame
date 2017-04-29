844df972fbac8590c1269bd782c71667424b83fd
HaakonJ <haakon@haakonjakobson.com>
2017-04-29 15:30:57 -0700

    added TeamKismetMathGame as well as created start of AdditionQuestion class.

---
+++ TeamKismetMathGame/TeamKismetMathGame/Models/AdditionQuestion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace TeamKismetMathGame.Models
+{
+    public class AdditionQuestion
+    {
+        [Key]
+        private int AQuestionID { get; set; }
+
+        private int AddAnswer { get; set; }
+
+        private int AddVariable { get; set; }
+
+        Random rnd = new Random();
+
+        //SkillTracker addCounter = new skillTracker();
+
+        //MathTracker FullCounter = new MathTracker();
+
+    }
+}
