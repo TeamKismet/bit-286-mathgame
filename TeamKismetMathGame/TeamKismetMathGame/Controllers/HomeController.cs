46f929291b9f2f5be9cf455b2333d1cee20f2d0b
Jonathan Lopez <JonL_2086@hotmail.com>
2017-05-08 03:29:59 -0700

    Created Student and Teacher Login, NewTeachAccount
    Login pages are created. Still some tweaking needed to the models and
    controllers to connect these views to the database.

--- TeamKismetMathGame/TeamKismetMathGame/Controllers/HomeController.cs
+++ TeamKismetMathGame/TeamKismetMathGame/Controllers/HomeController.cs
@@ -26,5 +26,10 @@ namespace TeamKismetMathGame.Controllers
 
             return View();
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
     }
 }
