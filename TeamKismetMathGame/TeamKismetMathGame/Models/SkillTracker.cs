dde795cc80a707cbd7fd63df478f3ea7d0dbf85a
Jonathan Lopez <JonL_2086@hotmail.com>
2017-05-10 13:26:35 -0700

    Uploading latest working project files

---
+++ TeamKismetMathGame/TeamKismetMathGame/Models/SkillTracker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using TeamKismetMathGame.Models;
+using System.Web.Mvc;
+
+namespace TeamKismetMathGame.Models
+{
+    public class SkillTracker
+    {
+        [Key]
+        public int SkillID { get; set; }
+
+        public int MathPoints { get; set; }
+
+        public int AdditionPoints { get; set; }
+
+        public int SubtractionPoints { get; set; }
+
+        public int GeometryPoints { get; set; }
+
+    }
+
+    public class AdditionSkill
+    {
+        
+    }
+}
