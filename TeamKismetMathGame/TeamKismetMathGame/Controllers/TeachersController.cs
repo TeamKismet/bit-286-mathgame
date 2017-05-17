eb4d075e997538901dff33858729c84043ea1d3b
Jonathan Lopez <JonL_2086@hotmail.com>
2017-05-16 23:19:27 -0700

    Fixed bug: Teachers Model validators and Data Type
    Teachers model were missing validators and data type for Password.
    Password was visible in New Teacher Account page.

--- TeamKismetMathGame/TeamKismetMathGame/Controllers/TeachersController.cs
+++ TeamKismetMathGame/TeamKismetMathGame/Controllers/TeachersController.cs
@@ -60,6 +60,11 @@ namespace TeamKismetMathGame.Controllers
                 return RedirectToAction("Index");
             }
 
+            //if (teacher.ConfirmPassword == teacher.Password)
+            //{
+            //    return View(db.Teachers);
+            //}
+
             return View(teacher);
         }
 
--- TeamKismetMathGame/TeamKismetMathGame/Models/Teacher.cs
+++ TeamKismetMathGame/TeamKismetMathGame/Models/Teacher.cs
@@ -12,20 +12,31 @@
 namespace TeamKismetMathGame.Models
 {
 
-using System;
+    using System;
     using System.Collections.Generic;
-    
-public partial class Teacher
-{
+    using System.ComponentModel.DataAnnotations;
 
-    public int Id { get; set; }
+    public partial class Teacher
+    {
 
-    public string FirstName { get; set; }
+        public int Id { get; set; }
 
-    public string LastName { get; set; }
+        [Required(ErrorMessage = "First name is required")]
+        public string FirstName { get; set; }
 
-    public string Password { get; set; }
+        [Required(ErrorMessage = "Last name is required")]
+        public string LastName { get; set; }
 
-}
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        //[Required(ErrorMessage = "Confirm Password is required")]
+        //[StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        //[DataType(DataType.Password)]
+        //[Compare("Password")]
+        //public string ConfirmPassword { get; set; }
+    }
 
 }
