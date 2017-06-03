a34d0f586165f480affbcd27abc7e66f6f0a0b61
Jonathan Lopez <JonL_2086@hotmail.com>
2017-06-02 17:55:48 -0700

    Updating Teachers model with added features
    Improving teacher account registration.

--- TeamKismetMathGame/TeamKismetMathGame/Models/Teacher.cs
+++ TeamKismetMathGame/TeamKismetMathGame/Models/Teacher.cs
@@ -18,7 +18,7 @@ namespace TeamKismetMathGame.Models
 
     public partial class Teacher
     {
-
+        [Key]
         public int Id { get; set; }
 
         [Required(ErrorMessage = "First name is required")]
@@ -27,16 +27,21 @@ namespace TeamKismetMathGame.Models
         [Required(ErrorMessage = "Last name is required")]
         public string LastName { get; set; }
 
+        [EmailAddress(ErrorMessage = "Please enter a vaild email.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+
         [Required(ErrorMessage = "Password is required")]
-        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        [StringLength(255, ErrorMessage = "Must be between 6 and 255 characters", MinimumLength = 6)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        //[Required(ErrorMessage = "Confirm Password is required")]
-        //[StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
-        //[DataType(DataType.Password)]
-        //[Compare("Password")]
-        //public string ConfirmPassword { get; set; }
+        [StringLength(255, ErrorMessage = "Must be between 6 and 255 characters", MinimumLength = 6)]
+        [Compare("Password", ErrorMessage ="Please confirm your password.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
     }
 
 }
